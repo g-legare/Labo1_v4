4c61487562dd1e1123a31a1132b2d3854e203c16
Guillaume Légaré <104878255+g-legare@users.noreply.github.com>
2022-05-14 23:38:50 -0400

    Update Labo1_test4.java

--- src/labo1_test4/Labo1_test4.java
+++ src/labo1_test4/Labo1_test4.java
@@ -14,7 +14,7 @@ public class Labo1_test4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-           System.out.println("Bonjour " + args[0] + " " + args[1] + " !");
+           System.out.println("Bonjour bonjour " + args[0] + " " + args[1] + " !");
     }
     
 }
