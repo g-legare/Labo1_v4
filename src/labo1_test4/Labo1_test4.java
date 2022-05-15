9b1fc67d7893dce79d4afcbd44ff9033faf4e1e2
macbookpro <macbookpro@mbps-computer.hitronhub.home>
2022-05-14 23:33:02 -0400

    Premier commit labo1 v4

---
+++ src/labo1_test4/Labo1_test4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labo1_test4;
+
+/**
+ *
+ * @author macbookpro
+ */
+public class Labo1_test4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           System.out.println("Bonjour " + args[0] + " " + args[1] + " !");
+    }
+    
+}
